ba316c74cd8a20999c14aed9e5570f1598b7f7af
Stefan <slessmann@posteo.de>
2019-02-15 15:30:28 -0500

    caps

--- db/migrate/20190208150431_create_table_user_rides.rb
+++ db/migrate/20190208150431_create_table_user_rides.rb
@@ -1,6 +1,6 @@
 class CreateTableUserRides < ActiveRecord::Migration[5.1]
   def change
-    create_table :User_rides do |t|
+    create_table :user_rides do |t|
     	t.integer :user_id
     	t.integer :ride_id
     	t.string :status_participation
